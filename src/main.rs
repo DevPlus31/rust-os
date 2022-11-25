3b7c3b913512aa6ec73177eeb8002bee521a542f
Mustapha Ben <devplus31@gmail.com>
2022-11-25 14:30:56 +0100

    add test

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 #![feature(custom_test_frameworks)]
 #![test_runner(crate::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
 mod vga_buffer;
@@ -10,6 +11,9 @@ mod vga_buffer;
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
@@ -27,4 +31,11 @@ fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+}
+
+#[test_case]
+fn trivial_assertion() {
+    print!("trivial assertion... ");
+    assert_eq!(1, 1);
+    println!("[ok]");
 }
