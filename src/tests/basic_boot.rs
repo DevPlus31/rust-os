ee15500bfd901efe7a77810c87c33b6a81dfa00c
Mustapha Ben <devplus31@gmail.com>
2022-12-03 21:03:41 +0100

    add testing

--- src/tests/basic_boot.rs
+++ src/tests/basic_boot.rs
@@ -1,6 +1,5 @@
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
-
+#![no_std]
+#![no_main]
 #![feature(custom_test_frameworks)]
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
@@ -10,24 +9,15 @@ use rust_os::println;
 
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    println!("Hello World{}", "!");
-
-    #[cfg(test)]
     test_main();
 
     loop {}
 }
 
-
-/// This function is called on panic.
-#[cfg(not(test))] 
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}", info);
-    loop {}
+fn test_runner(tests: &[&dyn Fn()]) {
+    unimplemented!();
 }
 
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_os::test_panic_handler(info)
